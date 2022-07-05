3ed97abb38017861f53b6b16da78996c0bfeb46d
Paula Solabre <p.solabre@fgupm.upm.es>
2022-07-05 14:51:20 +0200

    still not working

--- src/App.js
+++ src/App.js
@@ -3,24 +3,33 @@ import './App.css';
 import React from "react";
 import Button from '@mui/material/Button';
 
-let number=0;
-const contador = () => { number++ };
-
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+    let number=0;
+    //let estado = this.state;
+    // const contador = () => {
+    //     //e.preventDefault();
+    //     number++;
+    //     return(
+    //         <p>Hay {number} personas en el Pub.</p>
+    //     );
+    // };
+    function contador() {
+        number++;
+        return(
+            <p>Hay {number} personas en el Pub.</p>
+        )
+    }
 
-        <Button variant="outlined" onclick="contador()">Contador</Button>
-          <p>Hay {number} personas en el Pub.</p>
-          {/*<div className="sacarPorPantalla">*/}
-          {/*    <p>It is {new Date().toLocaleTimeString()}.</p>*/}
-          {/*</div>*/}
-
-      </header>
-    </div>
-  );
+    return(
+        <div className="App">
+            <header className="App-header">
+                <Button variant="outlined" onClick={ function(e) {
+                    this.contador();
+                }}
+                >Contador</Button>
+            </header>
+        </div>
+    )
 }
 
 export default App;
